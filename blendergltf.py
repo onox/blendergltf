0fe517b39809c5144466551ea7388314e73fc20c
onox <denkpadje@gmail.com>
2016-10-09 13:32:17 +0200

    Add assert statement for accessor.count
    If the material / texture of an object is messed up in Blender, a mesh
    can contain a primitive that has an "indices" member that points to an
    accessor that has a "count" value that is 0.
    
    "count" must be >= 1. See accessor.count in the glTF spec.
    
    Signed-off-by: onox <denkpadje@gmail.com>

--- blendergltf.py
+++ blendergltf.py
@@ -155,6 +155,7 @@ class Buffer:
                      component_type,
                      count,
                      type):
+            assert count >= 1, "Accessor '{}' has no count >= 1".format(name)
             self.name = name
             self.buffer = buffer
             self.buffer_view = buffer_view
